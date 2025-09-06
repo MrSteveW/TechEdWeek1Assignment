2d4fcae26094567d7cbaaad80b1d68a5533ee8db
MrSteveW <mrstevenwilliams@gmail.com>
2025-09-06 15:54:52 +0100

    Tweak contact chat

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function changeDarkMode() {
   document.body.style.backgroundColor = "#1d2935";
-  document.body.style.color = "white";
+  document.body.style.color = "#FFFFFF";
   const header = document.getElementById("header");
   header.style.backgroundColor = "#1d2935";
+  header.style.color = "#FFFFFF";
   const footer = document.getElementById("footer");
   footer.style.backgroundColor = "#1d2935";
+  footer.style.color = "#FFFFFF";
 }
